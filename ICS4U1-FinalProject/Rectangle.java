47758a11b7d46d6d3be1c36b20ffef287227ec96
Daniel Qian <95648807+danielqian0922@users.noreply.github.com>
2022-01-09 22:49:28 -0500

    First Copy of Game
    Added castles and background. 
    Also added some useful classes.

---
+++ ICS4U1-FinalProject/Rectangle.java
@@ -0,0 +1,221 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Class that creates a button with a fill color and border around it. 
+ * Used for all colorify buttons
+ * <p>
+ * Subclass of control, and implements highlighting
+ * 
+ * @author Jerry Zhu
+ * @version December 2021
+ */
+public class Rectangle extends Control    
+{
+    // Instance variables
+    private GreenfootImage image;
+    private int width;
+    private int height;
+    private int borderThickness;
+    private Color color;
+    private Color borderColor;
+    
+    /**
+     * Basic constructor to create a color button
+     * 
+     * @param color           Base color of the color button
+     * 
+     * @author Jerry Zhu
+     */
+    public Rectangle(Color color){
+        this.color = color;
+        width = 20;
+        height = 20;
+        image = new GreenfootImage(20, 20);
+        borderThickness = 2;
+        borderColor = Color.WHITE;
+        image.setColor (borderColor);
+        for (int i = 0; i < borderThickness; i++){
+            image.drawRect (0 + i, 0 + i, 20 - 1 - (i * 2), 20 - 1 - (i*2));
+        }
+        image.setColor(color);
+        image.fillRect(2, 2, 16, 16);
+        mouseOver = false;
+        clickable = true;
+        setImage(image);
+    }
+    
+    /**
+     * Similar to above, but with the ability to adjust width and height
+     * 
+     * @param color           Base color of the color button
+     * @param width           The width of the image
+     * @param height          The height of the image
+     * 
+     * @author Jerry Zhu
+     */
+    public Rectangle(Color color, int width, int height){
+        this.color = color;
+        this.width = width;
+        this.height = height;
+        image = new GreenfootImage(width, height);
+        borderThickness = 2;
+        borderColor = Color.WHITE;
+        image.setColor (borderColor);
+        for (int i = 0; i < borderThickness; i++){
+            image.drawRect (0 + i, 0 + i, width - 1 - (i * 2), height - 1 - (i*2));
+        }
+        image.setColor(color);
+        image.fillRect(borderThickness, borderThickness, width - borderThickness * 2, height - borderThickness * 2);
+        mouseOver = false;
+        clickable = true;
+        setImage(image);
+    } 
+    
+    /**
+     * Similar to above, but with the ability to set the button to animate on hover
+     * 
+     * @param color           Base color of the color button
+     * @param width           The width of the image
+     * @param height          The height of the image
+     * @param isClickable     Whether the button supports UI animations or not
+     * 
+     * @author Jerry Zhu
+     */
+    public Rectangle(Color color, int width, int height, boolean isClickable){
+        this.color = color;
+        this.width = width;
+        this.height = height;
+        image = new GreenfootImage(width, height);
+        borderThickness = 2;
+        borderColor = Color.WHITE;
+        image.setColor (borderColor);
+        for (int i = 0; i < borderThickness; i++){
+            image.drawRect (0 + i, 0 + i, width - 1 - (i * 2), height - 1 - (i*2));
+        }
+        image.setColor(color);
+        image.fillRect(borderThickness, borderThickness, width - borderThickness * 2, height - borderThickness * 2);
+        mouseOver = false;
+        clickable = isClickable;
+        setImage(image);
+    } 
+    
+    /**
+     * Similar to above, but with the ability to adjust border thickness
+     * 
+     * @param color           Base color of the color button
+     * @param width           The width of the image
+     * @param height          The height of the image
+     * @param borderThickness The thickness of the border around the button
+     * @param isClickable     Whether the button supports UI animations or not
+     * 
+     * @author Jerry Zhu
+     */
+    public Rectangle(Color color, int width, int height, int borderThickness, boolean isClickable){
+        this.color = color;
+        this.width = width;
+        this.height = height;
+        image = new GreenfootImage(width, height);
+        borderColor = Color.WHITE;
+        image.setColor (borderColor);
+        for (int i = 0; i < borderThickness; i++){
+            image.drawRect (0 + i, 0 + i, width - 1 - (i * 2), height - 1 - (i*2));
+        }
+        image.setColor(color);
+        image.fillRect(borderThickness, borderThickness, width - borderThickness * 2, height - borderThickness * 2);
+        mouseOver = false;
+        clickable = isClickable;
+        setImage(image);
+    } 
+    
+    /**
+     * Similar to above, but with the ability to adjust transparency
+     * 
+     * @param color           Base color of the color button
+     * @param width           The width of the image
+     * @param height          The height of the image
+     * @param borderThickness The thickness of the border around the button
+     * @param isClickable     Whether the button supports UI animations or not
+     * @param transparent     Whether or not the button is transparent
+     * 
+     * @author Jerry Zhu, Matthew Gong
+     */
+    public Rectangle(Color color, int width, int height, int borderThickness, boolean isClickable, boolean transparent){
+        this.color = color;
+        this.width = width;
+        this.height = height;
+        image = new GreenfootImage(width, height);
+        borderColor = Color.BLACK;
+        image.setColor (borderColor);
+        for (int i = 0; i < borderThickness; i++){
+            image.drawRect (0 + i, 0 + i, width - 1 - (i * 2), height - 1 - (i*2));
+        }
+        if(transparent){
+            image.setColor(color);
+            image.fillRect(borderThickness, borderThickness, width - borderThickness * 2, height - borderThickness * 2);
+        }
+        mouseOver = false;
+        clickable = isClickable;
+        setImage(image);
+    } 
+    
+    /**
+     * Most sophisticated constructor, adding the ability to adjust border color
+     * Similar to above, but with the ability to adjust transparency
+     * 
+     * @param color           Base color of the color button
+     * @param width           The width of the image
+     * @param height          The height of the image
+     * @param borderThickness The thickness of the border around the button
+     * @param borderColor     The color of the border around the button
+     * @param isClickable     Whether the button supports UI animations or not
+     * @param transparent     Whether or not the button is transparent
+     * 
+     * @author Jerry Zhu
+     */
+    public Rectangle(Color color, int width, int height, int borderThickness, Color borderColor, boolean isClickable, boolean transparent){
+        this.color = color;
+        this.width = width;
+        this.height = height;
+        image = new GreenfootImage(width, height);
+        image.setColor (borderColor);
+        for (int i = 0; i < borderThickness; i++){
+            image.drawRect (0 + i, 0 + i, 20 - 1 - (i * 2), 20 - 1 - (i*2));
+        }
+        image.setColor(color);
+        image.fillRect(borderThickness, borderThickness, width - borderThickness * 2, height - borderThickness * 2);
+        mouseOver = false;
+        clickable = isClickable;
+        setImage(image);
+    } 
+    
+    /**
+     * Update the image when the mouse is hovering over it by redrawing it and giving it a highlighted and enlarged effect
+     */
+    public void update(){
+        highlightedImage = reDraw (color, color, Color.WHITE, 2);
+        myImage = image;
+    }
+    
+    /**
+     * Redraw the highlighted and enlarged color button
+     * 
+     * @param color 
+     * @param back
+     */
+    public GreenfootImage reDraw (Color color, Color back, Color border, int pixelsLarger){
+        // Get new width and height
+        int newWidth = width + pixelsLarger * 2;
+        int newHeight = height + pixelsLarger * 2;
+        GreenfootImage resultImage = new GreenfootImage(newWidth, newHeight);
+        resultImage.setColor (borderColor);
+        for (int i = 0; i < borderThickness; i++){
+            resultImage.drawRect (0 + i, 0 + i, newWidth - 1 - (i * 2), newHeight - 1 - (i * 2));
+        }
+        resultImage.setColor(color);
+        resultImage.fillRect(borderThickness, borderThickness, newWidth - borderThickness * 2, newHeight - borderThickness * 2);
+        
+        // Return the result image
+        return resultImage;
+    }
+    
+}
---
+++ ICS4U1-FinalProject/TextButton.java
@@ -0,0 +1,244 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * A Generic Button to display text that is clickable. Owned by a World, which controls click
+ * capturing. Can also be used as a simple field to display text (set clickable to false).
+ * 
+ * Massively improved over the previous version! Now modular, supports highlighting, etc. To implement
+ * highlighting, ensure that this is subclassed to Control.
+ * 
+ * @author Jordan Cohen
+ * @version v0.6.0 (December 2021)
+ */
+public class TextButton extends Control
+{
+    // Declare private variables
+
+    private String buttonText;
+
+    private int padding;
+    private int highlightPadding;
+    private boolean fixedWidthEnabled;
+    private boolean active; // ability to set and store inactive state
+    private int fixedWidth;
+
+    private Color textColor;
+    private Color highlightedTextColor;
+    private Color backColor;
+    private Color highlightedBackColor;
+    private Color disabledColor;
+    private Color borderColor;
+    private SuperTextBox description;
+
+    private Font font;
+
+    /**
+     * Construct a TextButton with a given String at the default size
+     * 
+     * @param text  String value to display
+     * 
+     */
+    public TextButton (String text)
+    {
+        this(text, new Font ("Comic Sans MS", false, false, 16));
+        active = true;
+    }
+    
+    /**
+     * Construct a TextButton with a given String and a specified font
+     * 
+     * @param text  String value to display
+     * @param font  the font of the text, as a Font
+     * 
+     */
+    public TextButton (String text, Font font)
+    {
+        this (text, 8, true, font);
+    }
+
+    /**
+     * Construct a TextButton with a given String and a specified padding and size and clickability
+     * 
+     * @param text  String value to display
+     * @param padding  the padding around the text, as an integer
+     * @param clickable  boolean denoting if the user can click it
+     * @param font  the font of the text, as a Font
+     */
+    public TextButton (String text, int padding, boolean clickable, Font font){
+        this (text,  padding, 0, clickable, new Color (40, 40, 250), new Color (80, 80, 250), new Color (245, 245, 245), new Color (255, 255, 255), Color.BLACK, font);  
+    }
+
+    /**
+     * Primary constructor for objects of class TextButton - the most customizable version!
+     * 
+     * @param text
+     * @param textSize
+     * @param width         Width of desired button. Use 0 to automatically size around text.
+     */
+    public TextButton (String text, int padding, int width, boolean clickable, Color textColor, Color highlightedTextColor, Color backColor, Color highlightedBackColor, Color borderColor, Font font){
+
+        this.textColor = textColor;
+        this.highlightedTextColor = highlightedTextColor;
+        this.backColor = backColor;
+        this.highlightedBackColor = highlightedBackColor;
+        this.borderColor = borderColor;
+
+        this.font = font;
+        
+        disabledColor = new Color (100, 100, 100); // Grey
+
+        fixedWidth = width;
+        if (width == 0){
+            fixedWidthEnabled = false;
+        } else {
+            fixedWidthEnabled = true; 
+        }
+
+        this.clickable = clickable;
+        this.padding = padding;
+        
+        highlightPadding = padding + 2;
+        buttonText = text;
+        active = true;
+        
+        update();
+        setImage(myImage);
+    }
+
+    /**
+     * Function called upon the text box's addition to the world
+     */
+    public void addedToWorld (World w){
+        super.addedToWorld (w);
+        update();
+    }
+    
+    /**
+     * Changes the fixed width of the text box
+     * 
+     * @param width - the new int width
+     */
+    public void setFixedWidth (int width){
+        fixedWidthEnabled = true;
+        fixedWidth = width;
+        update();
+    }
+    
+    /**
+     * Changes if the text box is active or not
+     * 
+     * @param active - boolean
+     */
+    public void setActive (boolean active)
+    {
+        this.active = active;
+        update();
+    }
+
+    /**
+     * Gets if the text box is active
+     * 
+     * @return boolean - Whether the box is active
+     */
+    public boolean isActive (){
+        return active;
+    }
+
+    /**
+     * Gets the amount of padding of the text box
+     * 
+     * @return int - the padding
+     */
+    public int getPadding (){
+        return padding;
+    }
+
+    /**
+     * This update method will change the displayed text.
+     * 
+     * @param   text    The text to display, as a String
+     */
+    public void update (String text){
+        buttonText = text;
+        update();
+    }
+
+    /**
+     * Changes the color of the text box
+     */
+    public void setColor (Color text, Color highlighted){
+        this.textColor = text;
+        this.highlightedTextColor = highlighted;
+        update();
+    }
+    
+    /**
+     * Changes the font of the text box
+     */
+    public void setFont (Font font){
+        this.font = font;
+        update();
+    }
+
+    /**
+     * This primary update method will redraw this Actor's image based on current variable values
+     */
+    public void update ()
+    {
+
+        if (active)
+            myImage = drawTextBox(buttonText, textColor, backColor, borderColor, padding);
+        else
+            myImage = drawTextBox(buttonText, disabledColor, backColor, Color.BLACK, padding);
+        if (clickable)
+            highlightedImage = drawTextBox (buttonText, highlightedTextColor, backColor, Color.BLACK, highlightPadding);
+        else
+            highlightedImage = null;
+
+        if (clickable && mouseOver){
+            setImage(highlightedImage);
+        } else {
+            setImage (myImage);
+        }
+    }
+
+    /**
+     * 
+     * Draw a text box.
+     * 
+     */
+    public GreenfootImage drawTextBox (String text, Color color, Color back, Color border, int pad){
+        int height = font.getSize() + (3 * pad);
+        int width;
+        int textWidth = getStringWidth(font, text);
+        if (fixedWidthEnabled){           
+            width = fixedWidth;
+        } else {
+            width = textWidth + (pad * 2);//(font.getSize() * text.length()) + (pad * 2);
+        }
+        // Ensure an even width to keep text properly centered
+        if (width % 2 == 1){
+            width++;
+        }
+
+        // generate an image of the appropriate size
+        GreenfootImage resultImage = new GreenfootImage(width, height);
+        resultImage.setFont(font);
+
+        // fill in the background colour
+        resultImage.setColor (back);
+        resultImage.fill();
+
+        // Draw the border
+        resultImage.setColor (border); 
+        resultImage.drawRect (0, 0, width-1, height-1);
+
+        // Draw the text
+        resultImage.setColor (color);
+        resultImage.drawString(text, (width - textWidth)/2, resultImage.getHeight() - pad * 2);//pad + font.getSize() + 1); // in y: +1 because border
+        
+        // Done!
+        return resultImage;
+    }
+    
+}
