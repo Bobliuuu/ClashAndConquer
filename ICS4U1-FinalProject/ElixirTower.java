515cff33ae3f2ec7abaf83687a6b6316b09fb7f4
Fossaul <mmmzzz66g@gmail.com>
2022-01-12 11:02:14 -0500

    Added elixir bar

--- ICS4U1-FinalProject/ElixirTower.java
+++ ICS4U1-FinalProject/ElixirTower.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class GoldMine extends Building
+public class ElixirTower extends Building
 {
     /**
      * Act - do whatever the GoldMine wants to do. This method is called whenever
--- ICS4U1-FinalProject/Level.java
+++ ICS4U1-FinalProject/Level.java
@@ -3,15 +3,25 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Level here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Coordinates: 
+ * Bridges: (127, 385), (673, 385), (398, 385)
+ * End of road: (397, 170):
+ * Road turns: (128,590), (674, 590) [my side], (128, 170), (672, 170)
+ * 
+ * @author Jerry Zhu
+ * @version January 2022
  */
 public class Level extends World
 {
+    // Instance varaibles
     private Image levelMap;
+    private Image elixir;
+    private Castle myCastle;
+    private Castle enemyCastle;
+    private ElixirBar elixirBar;
+    
     /**
-     * Constructor for objects of class Level.
-     * 
+     * Level world constructor
      */
     public Level()
     {    
@@ -20,10 +30,20 @@ public class Level extends World
         levelMap = new Image(new GreenfootImage("background.png"));
         addObject(levelMap, getWidth()/2, getHeight()/2);
         
-        Castle myCastle = new Castle(false);
-        addObject(myCastle, 400, 720);
+        myCastle = new Castle(false);
+        addObject(myCastle, 400, 680);
         
-        Castle enemyCastle = new Castle(true, 130, 130);
-        addObject(enemyCastle, 400, 60);
+        enemyCastle = new Castle(true, 130, 130);
+        addObject(enemyCastle, 400, 100);
+        
+        elixir = new Image(new GreenfootImage("elixirbar.png"));
+        addObject(elixir, 400, 770);
+
+        elixirBar = new ElixirBar();
+        addObject(elixirBar, 400, 770);
+    }
+    
+    public void act(){
+        elixirBar.addElixir();
     }
 }
