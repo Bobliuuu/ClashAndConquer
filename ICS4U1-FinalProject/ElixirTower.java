f35328214829a951759a5d5bebb583156c839fe8
Matthew <m4ttp0w3r@gmail.com>
2022-01-24 13:55:10 -0500

    Added Elixir Tower Functionality
    The speed is a variable but the default should be around 0.004 to 0.008

--- ICS4U1-FinalProject/ElixirTower.java
+++ ICS4U1-FinalProject/ElixirTower.java
@@ -8,18 +8,34 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class ElixirTower extends Building
 {
+    private double elixirSpeed;
+    private boolean increasedSpeed;
+    
+    public ElixirTower(double speed, boolean isEnemy){
+        this.isEnemy = isEnemy;
+        elixirSpeed = speed;
+        this.health = 175;
+        setImage("elixircollectorlevel1.png");
+        getImage().scale(75, 75);
+        increasedSpeed = false;
+    }
+    
     /**
-     * Act - do whatever the GoldMine wants to do. This method is called whenever
+     * Act - do whatever the ElixirTower wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+        if(!increasedSpeed){
+            getWorld().getObjects(ElixirBar.class).get(0).increaseElixirSpeed(elixirSpeed);
+            increasedSpeed = true;
+        }
     }
     
     public void subtractHealth(int value){
         health -= value;
         if (health <= 0){
+            getWorld().getObjects(ElixirBar.class).get(0).decreaseElixirSpeed(elixirSpeed);
             ((Level)getWorld()).removeObject(this);
         }
     }
