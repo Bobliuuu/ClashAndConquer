08ee7ddae21c89e1cafe62fde3d3f9d8f498c93f
Fossaul <mmmzzz66g@gmail.com>
2022-01-31 11:52:43 -0500

    fixed images

--- ICS4U1-FinalProject/rotatescript.py
+++ ICS4U1-FinalProject/rotatescript.py
@@ -4,6 +4,8 @@ troopName = "Skeleton" # Change to troop name accordingly
 # Get move and attack paths
 movePath = "images/Troops/" + troopName + "/" + troopName + "Move"
 attackPath = "images/Troops/" + troopName + "/" + troopName + "Attack"
+Min = 0
+Max = 12
 
 while True:
     try:
@@ -12,12 +14,14 @@ while True:
     except ValueError:
         print("Invalid angle.")
 
-for i in range(12, 14):
+# Move images
+for i in range(Min, Max):
     img = Image.open(movePath + str(i) + ".png")
     ret = img.rotate(angle)
     ret.save(movePath + str(i) + ".png")
 
-for i in range(12, 14):
+# Attack images
+for i in range(Min, Max):
     img = Image.open(attackPath + str(i) + ".png")
     ret = img.rotate(angle)
     ret.save(attackPath + str(i) + ".png")
